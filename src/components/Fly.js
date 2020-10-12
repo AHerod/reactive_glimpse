28c2cfaf96f5bf21f44bba7c218aa6fbbe6ccefc
Agnieszka Herod <agn.j.herod@gmail.com>
2020-10-12 19:40:16 +0200

    add Red Eye Model

--- src/components/Fly.js
+++ src/components/Fly.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import ReactDOM from 'react-dom'
 import React, {Suspense, useState, useCallback, useEffect, useRef, useMemo} from 'react'
-import { Canvas, useFrame } from 'react-three-fiber'
+import {Canvas, useFrame} from 'react-three-fiber'
 
 // Components & Effects
 import Effects from './Effects'
@@ -10,8 +10,9 @@ import Quote from "./Quote";
 import EyeModel from "./EyeModel";
 import {Text, Stars, OrbitControls, Html} from "drei";
 import "../App.scss";
+import RedEyeModel from "./RedEyeModel";
 
-function Bubble({ count, mouse }) {
+function Bubble({count, mouse}) {
   const mesh = useRef()
   const dummy = useMemo(() => new THREE.Object3D(), [])
   const particles = useMemo(() => {
@@ -23,14 +24,14 @@ function Bubble({ count, mouse }) {
       const xFactor = -20 + Math.random() * 40
       const yFactor = -20 + Math.random() * 40
       const zFactor = -20 + Math.random() * 40
-      temp.push({ t, factor, speed, xFactor, yFactor, zFactor, mx: 0, my: 0 })
+      temp.push({t, factor, speed, xFactor, yFactor, zFactor, mx: 0, my: 0})
     }
     return temp
   }, [count])
 
   useFrame((state) => {
     particles.forEach((particle, i) => {
-      let { t, factor, speed, xFactor, yFactor, zFactor } = particle
+      let {t, factor, speed, xFactor, yFactor, zFactor} = particle
       t = particle.t += speed / 2
       const a = Math.cos(t) + Math.sin(t * 1) / 10
       const b = Math.sin(t) + Math.cos(t * 2) / 10
@@ -52,8 +53,8 @@ function Bubble({ count, mouse }) {
   return (
     <>
       <instancedMesh ref={mesh} args={[null, null, count]}>
-        <boxBufferGeometry attach="geometry" args={[.5, .5, .5]} />
-        <meshPhongMaterial attach="material" specular='#64052C' color="#33FFBD" />
+        <boxBufferGeometry attach="geometry" args={[.5, .5, .5]}/>
+        <meshPhongMaterial attach="material" specular='#64052C' color="#33FFBD"/>
       </instancedMesh>
     </>
   )
@@ -62,11 +63,12 @@ function Bubble({ count, mouse }) {
 function Fly() {
   const [hiddenQuote, setHiddenQuote] = useState(true);
   const [hiddenCubes, setHiddenCubes] = useState(true);
+  const [hiddenRedEye, setHiddenRedEye] = useState(true);
   const [hovered] = useState(false)
   const [glitch, setGlitch] = useState(false)
   const [hoverColor, setHoverColor] = useState(true);
   const mouse = useRef([0, 0])
-  const onMouseMove = useCallback(({ clientX: x, clientY: y }) => (mouse.current = [x - window.innerWidth / 2, y - window.innerHeight / 2]), [])
+  const onMouseMove = useCallback(({clientX: x, clientY: y}) => (mouse.current = [x - window.innerWidth / 2, y - window.innerHeight / 2]), [])
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
 
   useEffect(() => {
@@ -80,17 +82,26 @@ function Fly() {
     <Canvas
       pixelRatio={Math.min(2, isMobile ? window.devicePixelRatio : 1)}
       camera={{position: [-3, 1, 12], fov: 80}}
-      onCreated={({ gl }) => {
+      onCreated={({gl}) => {
         gl.setClearColor(new THREE.Color('#020207'))
       }} onMouseMove={onMouseMove}>
 
-      <fog attach="fog" args={['lightblue', 15, 150]} />
+      <fog attach="fog" args={['lightblue', 15, 150]}/>
       <ambientLight intensity={0.3}/>
       <pointLight position={[-10, 0, -20]} intensity={.1}/>
       <pointLight position={[-20, -10, -40]} intensity={.1}/>
       <group>
         <Text
-          color={ hiddenQuote ? 'pink' : '#33FFBD'}
+          color={hiddenRedEye ? 'pink' : '#33FFBD'}
+          position={[-10, 7, -5]}
+          fontSize={1}
+          onClick={() => setHiddenRedEye(!hiddenRedEye)}
+          depthOffset={10}
+        >
+          Heterochromia
+        </Text>
+        <Text
+          color={hiddenQuote ? 'pink' : '#33FFBD'}
           position={[0, 10, -3]}
           fontSize={2}
           onClick={() => setHiddenQuote(!hiddenQuote)}
@@ -99,39 +110,42 @@ function Fly() {
           Quote
         </Text>
         <Text
-          color={ hiddenCubes ? 'pink' : '#33FFBD'}
+          color={hiddenCubes ? 'pink' : '#33FFBD'}
           position={[10, 5, -1]}
           fontSize={3}
           onClick={() => setHiddenCubes(!hiddenCubes)}
-
         >
           Cubes
         </Text>
       </group>
       {
-        hiddenQuote &&
-          <Quote />
+        !hiddenQuote &&
+        <Quote/>
       }
       {
-        hiddenCubes &&
-        <group className={'swarmScreen'} style={{ width: '100%', height: '100%' }} onMouseMove={onMouseMove}>
-          <Bubble mouse={mouse} count={300} args={[.1,.1,.1]} />
+        !hiddenCubes &&
+        <group className={'swarmScreen'} style={{width: '100%', height: '100%'}} onMouseMove={onMouseMove}>
+          <Bubble mouse={mouse} count={300} args={[.1, .1, .1]}/>
         </group>
       }
       <Suspense
         fallback={
           null
         }>
-        <EyeModel scale={[.2,.2,.2]} onClick ={() => setGlitch(!glitch)} />
+        {
+          !hiddenRedEye &&
+          <RedEyeModel scale={[.2, .2, .2]} onClick={() => setGlitch(!glitch)} position={[-10, 0, 0]}/>
+        }
+        <EyeModel scale={[.2, .2, .2]} onClick={() => setGlitch(!glitch)}/>
       </Suspense>
-      <Particles count={isMobile ? 5000 : 10000} mouse={mouse} />
-      <Effects down={glitch} />
-      <OrbitControls />
+      <Particles count={isMobile ? 5000 : 10000} mouse={mouse}/>
+      <Effects down={glitch}/>
+      <OrbitControls/>
     </Canvas>
   )
 }
 
-ReactDOM.render(<Fly />, document.getElementById('root'))
+ReactDOM.render(<Fly/>, document.getElementById('root'))
 
 
 export default Fly;
